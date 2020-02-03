3bbd40b55af7cbcfbff67e5f5428585c06648bb5
AntoMonti <a.matviishyn@hotmail.com>
2020-02-03 20:33:23 +0200

    create routes for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :contacts, only: [:new, :create], path_names: { :new => '' }
 end
