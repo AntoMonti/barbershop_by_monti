7605581009b42086203c910fb944f90a1b4c0a8e
AntoMonti <a.matviishyn@hotmail.com>
2020-02-03 22:13:35 +0200

    create route for barbers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'about' => 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :barbers
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   root 'home#index'
 end
