6aa31fa457cec196841d965582190dfc1fe161bf
AntoMonti <a.matviishyn@hotmail.com>
2020-02-03 21:27:06 +0200

    create rout for about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'about' => 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
 end
