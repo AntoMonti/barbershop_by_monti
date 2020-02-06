a3f9bf56c6c759ffc7e626c122a2ce702d42f3fb
AntoMonti <a.matviishyn@hotmail.com>
2020-02-06 15:46:02 +0200

    create def new, create and private

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,21 @@
 class AppointmentsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.valid?
+      @appointment.save
+    else
+      render action: 'new'
+    end
+  end
+
+private
+  
+  def appointment_params
+    params.require(:appointment).permit(:name, :phone, :barber, :datetime)
+  end
+
 end
