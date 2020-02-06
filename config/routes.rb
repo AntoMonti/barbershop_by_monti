b4f86a477b8735fb6673d49b4be287fb9ccc8d37
AntoMonti <a.matviishyn@hotmail.com>
2020-02-06 14:34:16 +0200

    adding referent way for page 'services'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'about' => 'pages#about'
+  get 'services' => 'pages#services'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :barbers
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
