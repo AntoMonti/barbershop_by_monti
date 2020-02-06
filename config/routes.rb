ade72d0e20f62cefe489268796d84f30b6ced9e3
AntoMonti <a.matviishyn@hotmail.com>
2020-02-06 14:46:48 +0200

    adding referent way for appointments resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'services' => 'pages#services'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :barbers
+  resource :appointments, only: [:new, :create], path_names: { :new => ''}
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   root 'home#index'
 end
